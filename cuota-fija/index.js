57e2b660a498926da3fe61fd5f17a28d935bdc7d
Danielru <danielru@MacBook-Pro-de-Daniel.local>
2021-03-10 02:09:42 -0500

    cuota fija casi terminado

--- cuota-fija/index.js
+++ cuota-fija/index.js
@@ -1,19 +1,49 @@
-// variables
-const Monto 
-const Interes
-const Tiempo
-
-// mostrar en la tabla
 const Datos = document.getElementById('tabla-de-amortizacion')
-// traer datos
-    // me pinta cada fila de a 1
-    Datos.innerHTML=''
-    Datos.innerHTML+= `
-    
+const calcular = document.getElementById("enviar")
+calcular.addEventListener('submit', generarAmortizacion)
+// Funcion generar amortización.
+function generarAmortizacion (e) {
+     e.preventDefault()
+     while(Datos.firstChild){
+        Datos.removeChild(Datos.firstChild);
+    }
+    // variables
+        let Monto= document.getElementById("Monto").value
+        const Interes= document.getElementById("Interes").value
+        const Tiempo= document.getElementById("Tiempo").value
+        let PagoInteres = 0
+        let PagoCapital = 0
+        let Cuota= 0
+    // declaramos un arreglo para la fechas
+        let Fechas = []
+        let FechaActual = Date.now()
+        let MesActual = moment(FechaActual)
+        MesActual.add(1, 'month');
+    // formula para calcular la cuota
+        Cuota = Monto * (Math.pow(1+Interes/100, Tiempo)
+                      *Interes/100)/(Math.pow(1+Interes/100, Tiempo)-1)
+    // llenar los datos de la tabla
+       for(let i = 1; i <= Tiempo; i++){
+        PagoInteres = parseFloat(Monto*(Interes/100));
+        PagoCapital = Cuota - PagoInteres;
+        Monto = parseFloat(Monto- PagoCapital)
 
-                 
-                    
-                 
-        
-    `
- 
+        //Formato fechas
+        Fechas[i] = MesActual.format('DD-MM-YYYY');
+        MesActual.add(1, 'month');
+
+        //insertar datos en la tabla
+       const llenartabla = document.createElement('tr')
+        llenartabla.innerHTML=`
+    
+            <td>${Fechas[i]}</td>
+            <td>${Cuota.toFixed(3)}</td>
+            <td>${PagoCapital.toFixed(3)}</td>
+            <td>${PagoInteres.toFixed(3)}</td>
+            <td>${Monto.toFixed(3)}</td>
+        `
+    Datos.appendChild(llenartabla)
+    }
+    //resetiamos el formulario
+    calcular.reset()
+}
--- cuota-variable/index.js
+++ cuota-variable/index.js
@@ -1,16 +1,9 @@
 // variables
-const Monto 
-const Interes
-const Tiempo
+const Monto= document.getElementById("Monto")
+const Interes= document.getElementById("Interes")
+const Tiempo= document.getElementById("Tiempo")
+const calcular = document.getElementById("enviar")
 // mostrar en la tabla
 const Datos = document.getElementById('tabla-de-amortizacion')
-// traer datos
-    // me pinta cada fila de a 1
-    Datos.innerHTML=''
-    Datos.innerHTML+= `
-    
-
-                 
-    `               
                  
        
